9ab3bd6dde46375d72dec13cfe8a789bf7ed3d17
NayantaraPrem <premnaya@ug170.eecg>
2015-11-30 15:45:44 -0500

    Remove TRUE/FALSE from codegen.h

--- Starter3/Starter3/codegen.h
+++ Starter3/Starter3/codegen.h
@@ -1,8 +1,6 @@
 #ifndef CODEGEN_H_
 #define CODEGEN_H_
 #include "ast.h"
-#define TRUE "1.0"
-#define FALSE "-1.0"
 #define NUM_TEMP_REGS 16
 void genCode(node* ast, char* temp = NULL);
 void init_codegen(node* ast);
